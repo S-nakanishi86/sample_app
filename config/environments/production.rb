91b0e12613c46f8b2b29e066bba40f0322a62c34
SHOTA <s_nkns_1@yahoo.co.jp>
2019-02-14 22:44:32 +0900

    Add account activation5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,13 +69,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'https://shielded-badlands-27162.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.perform.deliveries = true
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
+    :domain => 'herokuapp.com',
     :enable_starttls_auto => true
   }
 
